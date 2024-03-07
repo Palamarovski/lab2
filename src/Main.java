f13d253a9494244e93a37b97a01e9df2c254d645
Vasia Pupkin <vpupkin@mail.com>
2024-03-07 11:53:29 +0200

    added files

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a=51;
+        int b=2;
+        double y= (double) a/b;
+        double u= a%b;
+        System.out.println(y + " " + u);
+
+        int e=45;
+        int x=e/10;
+        int i=e%10;
+        int p=x+i;
+        System.out.println(p);
+
+        double t=13.7;
+        int o=(int) (t+0.5);
+        char l= 'n';
+        System.out.println(o);
+    }
+
+}
